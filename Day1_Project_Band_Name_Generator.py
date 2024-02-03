3e876c90624cf54e42d3a8e9046e01fb06a32711
Kunj Gopal Shah <72154374+kunjshah0703@users.noreply.github.com>
2024-02-03 13:46:20 +0530

    Update Day1_Project_Band_Name_Generator.py

--- Day1_Project_Band_Name_Generator.py
+++ Day1_Project_Band_Name_Generator.py
@@ -1,4 +1,7 @@
+"""Day 1 Project: Generate Band Name based on city and pet name"""
+
 print("Welcome to the Band Name Generator.")
-a = input("What's the name of the city you grew up in? \n")
-b = input("What's your pet's name? \n")
-print(f"Your band name could be {a} {b}")
+city_name = input("What's the name of the city you grew up in? \n")
+pet_name = input("What's your pet's name? \n")
+#print(f"Your band name could be {a} {b}")
+print("Your band name could be " + city_name + " " + pet_name)
